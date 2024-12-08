6e9e52c3d531e095b7687902addd2ecd79ec557f
Moin9056 <u2024612@giki.edu.pk>
2024-12-08 14:51:39 +0500

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <string>
 using namespace std;
+//function for verifying age of voter
+int voterAge(int age)
+if(age>=18)
+cout<<"Your can vote"<<endl;
+else{
+cout<<"you are under age"<<endl;
+}
 
 int main (){
 
