185f96a0002d797ee3e304487faf8e60f8be95b7
Moin9056 <u2024612@giki.edu.pk>
2024-12-13 03:28:36 +0500

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -11,6 +11,38 @@ int votes;
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void castvote(Candidate candidates[], int candidateCount) {
+    int id;
+    cout << "Enter the candidate ID you want to vote for: ";
+    safeInput(id);
+
+    for (int i = 0; i < candidateCount; i++) {
+        if (candidates[i].id == id) {
+            candidates[i].votes++;
+            cout << "Vote casted successfully.\n";
+            return;
+        }
+    }
+    cout << "Invalid candidate ID. Please try again.\n";
+}
+
       
 
 int main (){
